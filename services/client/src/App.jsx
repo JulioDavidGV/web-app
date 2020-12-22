6ffe23971d4cc77f2861d6ede77430f0e8a9f0c4
david <JulioDavidGV>
2020-12-22 09:52:17 -0500

    react auth 1

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 
+import { Route, Switch } from 'react-router-dom';
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
 import About from './components/About';
 import NavBar from './components/NavBar';
 import Form from './components/Form';
-
 
 class App extends Component {
     constructor() {
@@ -25,6 +24,8 @@ class App extends Component {
         };
         this.addUser = this.addUser.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
+        this.handleFormChange = this.handleFormChange.bind(this);
     };
     componentDidMount() {
         this.getUsers();
@@ -52,6 +53,29 @@ class App extends Component {
         obj[event.target.name] = event.target.value;
         this.setState(obj);
     };
+    handleUserFormSubmit(event) {
+        event.preventDefault();
+        const formType = window.location.href.split('/').reverse()[0];
+        let data = {
+            email: this.state.formData.email,
+            password: this.state.formData.password,
+        };
+        if (formType === 'register') {
+            data.username = this.state.formData.username;
+        }
+        const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`
+        axios.post(url, data)
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((err) => { console.log(err); });
+    };
+    handleFormChange(event) {
+        const obj = this.state.formData;
+        obj[event.target.name] = event.target.value;
+        this.setState(obj);
+        //console.log(this.state.formData);
+    };
     render() {
         return (
             <div>
@@ -61,7 +85,24 @@ class App extends Component {
                         <div className="columns">
                             <div className="column is-half">
                                 <br />
+                                {/* new */}
                                 <Switch>
+                                    <Route exact path='/register' render={() => (
+                                        <Form
+                                            formType={'Register'}
+                                            formData={this.state.formData}
+                                            handleUserFormSubmit={this.handleUserFormSubmit}
+                                            handleFormChange={this.handleFormChange}  // nuevo
+                                        />
+                                    )} />
+                                    <Route exact path='/login' render={() => (
+                                        <Form
+                                            formType={'Login'}
+                                            formData={this.state.formData}
+                                            handleUserFormSubmit={this.handleUserFormSubmit}
+                                            handleFormChange={this.handleFormChange}  // nuevo
+                                        />
+                                    )} />
                                     <Route exact path='/' render={() => (
                                         <div>
                                             <h1 className="title is-1">All Users</h1>
@@ -77,18 +118,6 @@ class App extends Component {
                                         </div>
                                     )} />
                                     <Route exact path='/about' component={About} />
-                                    <Route exact path='/register' render={() => (
-                                        <Form
-                                            formType={'Register'}
-                                            formData={this.state.formData}
-                                        />
-                                    )} />
-                                    <Route exact path='/login' render={() => (
-                                        <Form
-                                            formType={'Login'}
-                                            formData={this.state.formData}
-                                        />
-                                    )} />
                                 </Switch>
                             </div>
                         </div>
